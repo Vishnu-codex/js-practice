666c79e398e727e8914d99c8c357690d9ec36ab3
c-vishnu <cvishnuofficial@gmail.com>
2024-11-02 01:08:19 +0530

    math1

---
+++ Addition.js
@@ -0,0 +1,17 @@
+//Arrow Function
+const add = (a, b) => {
+    let c = a + b;
+    return c
+}
+//normal funtion
+function addition (a, b) {
+    let c = a + b;
+    return c
+}
+
+
+let sum = add (10, 3)
+console.log("1.SUM = ", sum);
+
+let data = addition(100, 1000);
+console.log("2.SUM = ", data);
